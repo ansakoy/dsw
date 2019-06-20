49427e6dc2272b1eabda9b584ec5299fa50f6821
ansakoy <ansakoy@gmail.com>
2019-06-20 13:32:35 +0300

    translating

--- dsw/settings.py
+++ dsw/settings.py
@@ -138,7 +138,8 @@ LANG_INFO = dict(django.conf.locale.LANG_INFO, **EXTRA_LANG_INFO)
 django.conf.locale.LANG_INFO = LANG_INFO
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+# LANGUAGE_CODE = 'en-us'
 # LANGUAGE_CODE = 'hy'
 
 TIME_ZONE = 'UTC'
